b18e4486362a2f963e60801f58aa4cfe02c121bb
Michael Samoylov <michael.samoylov@gmail.com>
2015-06-12 22:26:50 +0300

    Username fix

--- project_name/settings/production.py
+++ project_name/settings/production.py
@@ -3,7 +3,6 @@ from {{ project_name }}.settings.staging import *   # noqa
 # There should be only minor differences from staging
 
 DATABASES['default']['NAME'] = '{{ project_name }}_production'
-DATABASES['default']['USER'] = '{{ project_name }}_production'
 
 EMAIL_SUBJECT_PREFIX = '[{{ project_name|title }} Prod] '
 
